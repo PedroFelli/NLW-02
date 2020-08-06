9d9271ac06746e9ba1b96f7910a3656c442e3025
Pedro Fellipe Mendes Amaral - Analista de Sistemas <pedro.amaral@unievangelica.edu.br>
2020-08-06 11:12:54 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes';
+
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+console.log('Server runing on port 3333!')
+app.listen(3333);
